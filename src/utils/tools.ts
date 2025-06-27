c5b180b4e1284b24f028c49b623d904652af95cc
Kon Spirin <kon@mail.com>
2025-06-27 15:10:24 +0300

    cw-41:db jest testing methods from tools.ts

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -3,3 +3,24 @@
 export const getRandomNumber = (start:number, finish:number) => {
     return Math.trunc(Math.random()*(finish - start) + start);
 }
+
+export const reverseArray = (arr:number[]) =>{
+    arr.reverse()
+    return arr;
+}
+
+export const div = (a:number,b:number) => {
+    if(b === 0) throw new Error("Dividing by zero!")
+    return a/b;
+}
+
+export const echo = (data:string)=> {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            if(data)
+                resolve(data);
+            else reject(new Error('Error'))
+        }, 1000)
+    })
+}
